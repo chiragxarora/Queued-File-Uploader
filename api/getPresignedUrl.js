e6af1dbfa6f3e9b5ad638f3f5243f905ab572c04
Chirag Arora <chiragxarora@users.noreply.github.com>
2025-06-11 13:16:30 +0530

    vercel config

--- api/getPresignedUrl.js
+++ api/getPresignedUrl.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-
+  console.log("GET request received");
   const { filename, filetype } = req.query;
 
   if (!filename || !filetype) {
